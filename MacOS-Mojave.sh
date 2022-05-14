3257d5f60d3ccc75ab1e7e9ad06620a8a018413b
gr026a1 <105536019+gr026a1@users.noreply.github.com>
2022-05-14 13:25:23 +0700

    Update MacOS-Mojave.sh

--- MacOS-Mojave.sh
+++ MacOS-Mojave.sh
@@ -1,6 +1,6 @@
 echo "Checking Virtualization..."
 vtx=$(egrep -i '^flags.*(vmx|svm)' /proc/cpuinfo | wc -l)
-if [ $vtx = 0 ] ; then echo "[Error] Your PC Not Support VT-x / AMD-v. Press Ctrl+C to quit"&& sleep 2711
+if [ $vtx = 1 ] ; then echo "[Error] Your PC Not Support VT-x / AMD-v. Press Ctrl+C to quit"&& sleep 2711
 fi
 echo Installing Software...
 sudo apt update -y
